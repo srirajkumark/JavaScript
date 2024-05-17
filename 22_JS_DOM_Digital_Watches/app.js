424fdd5f29a8973e7325335962d2444304900dba
SRIRAJKUMAR K <ksrirajk@gmail.com>
2024-05-17 10:39:39 +0530

    Updated HTML & JS Code

--- 22_JS_DOM_Digital_Watches/app.js
+++ 22_JS_DOM_Digital_Watches/app.js
@@ -2,8 +2,28 @@
 let indianWatch = () => {
     let options = {timeZone : 'Asia/Kolkata'};
     let currentDate = new Date().toLocaleDateString('en-US',options);
-    let currentTime = new Date().toLocaleYimeString('en-US',options);
+    let currentTime = new Date().toLocaleTimeString('en-US',options);
     document.querySelector('#indian-date').innerText = currentDate;
     document.querySelector('#indian-time').innerText = currentTime;
 };
-indianWatch();
+setInterval(indianWatch, 1000);
+
+// usa watch
+let usaWatch = () => {
+    let options = {timeZone : 'America/New_York'};
+    let currentDate = new Date().toLocaleDateString('en-US',options);
+    let currentTime = new Date().toLocaleTimeString('en-US',options);
+    document.querySelector('#usa-date').innerText = currentDate;
+    document.querySelector('#usa-time').innerText = currentTime;
+};
+setInterval(usaWatch, 1000);
+
+// china watch
+let chinaWatch = () => {
+    let options = {timeZone : 'Asia/Shanghai'};
+    let currentDate = new Date().toLocaleDateString('en-US',options);
+    let currentTime = new Date().toLocaleTimeString('en-US',options);
+    document.querySelector('#china-date').innerText = currentDate;
+    document.querySelector('#china-time').innerText = currentTime;
+};
+setInterval(chinaWatch, 1000);
