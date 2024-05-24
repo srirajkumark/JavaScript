867aeacf0f794a311361a317d8b3897bf7c1a013
Srirajkumar <ksrirajk@email.com>
2024-05-24 19:52:14 +0530

    Math Table HTML & JS Update

--- 23_JS_DOM_Math_Table/app.js
+++ 23_JS_DOM_Math_Table/app.js
@@ -3,4 +3,22 @@
 let numberEl = document.querySelector('#req-number');
 numberEl.addEventListener('keyup', function(){
     let theNumber = Number(numberEl.value);
+    document.querySelector('#number').innerText = theNumber;
+    calculate();
 });
+
+let rangeEl = document.querySelector('#multiplier');
+rangeEl.addEventListener('input', function(){
+    let selectedMultiplier = Number(rangeEl.value);
+    document.querySelector('#multi').innerText = selectedMultiplier;
+    calculate();
+});
+
+// calculate
+
+let calculate = () => {
+    let finalNumber = Number(document.querySelector('#number').innerText);
+    let finalMultiplier = Number(document.querySelector('#multi').innerText);
+    let mathResult = finalNumber * finalMultiplier;
+    document.querySelector('#result').innerText = mathResult;
+};
