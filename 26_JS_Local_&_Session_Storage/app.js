85910b47562282c136829a67d9067382dba42467
Srirajkumar <ksrirajk@email.com>
2024-05-28 22:59:11 +0530

    Updated JS Code

--- 26_JS_Local_&_Session_Storage/app.js
+++ 26_JS_Local_&_Session_Storage/app.js
@@ -9,7 +9,7 @@ which can be saved and accessed even after closing the browser
 
     => setItem('key', value); // create / update
     => getItem('key'); // read
-    => delete('key'); // delete
+    => removeItem('key'); // delete
     => clear();
 
 
@@ -20,7 +20,36 @@ which can be saved and accessed only when browser is running
 
     => setItem('key', value); // create / update
     => getItem('key'); // read
-    => delete('key'); // delete
+    => removeItem('key'); // delete
     => clear();
 
 */
+
+// Local Storage for simple string
+
+let empName = 'John';
+
+// set
+localStorage.setItem('name', empName);
+
+// get
+console.log(localStorage.getItem('name'));
+
+// delete the key
+localStorage.removeItem('name');
+
+// clear
+localStorage.clear();
+
+// local storage with an array
+let colors = ['white', 'black', 'silver', 'purple', 'yellow'];
+localStorage.setItem('colors', colors);
+
+let newColors = [   {id : 1, name : 'white'}, 
+                    {id : 2, name : 'silver'},
+                    {id : 3, name : 'yellow'},
+                    {id : 4, name : 'purple'},
+                ];
+
+localStorage.setItem('newcolors', newColors);
+
