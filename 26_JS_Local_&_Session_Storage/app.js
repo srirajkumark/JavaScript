8c3543d0c335bcca3f617e51f3b33a71d041c1f1
SRIRAJKUMAR K <ksrirajk@gmail.com>
2024-05-28 13:34:32 +0530

    Added JS Files

---
+++ 26_JS_Local_&_Session_Storage/app.js
@@ -0,0 +1,26 @@
+/*
+Storage
+------------
+
+-> Local Storage
+which can be saved and accessed even after closing the browser
+
+-> string -> JSON.stringify(), parse()
+
+    => setItem('key', value); // create / update
+    => getItem('key'); // read
+    => delete('key'); // delete
+    => clear();
+
+
+-> Sesson Storage
+which can be saved and accessed only when browser is running
+
+-> string -> JSON.stringify(), parse()
+
+    => setItem('key', value); // create / update
+    => getItem('key'); // read
+    => delete('key'); // delete
+    => clear();
+
+*/
