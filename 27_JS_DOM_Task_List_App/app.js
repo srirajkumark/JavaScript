7da13393b58fdffabe18d7d6461aebe50afe8757
Srirajkumar <ksrirajk@email.com>
2024-05-31 22:36:27 +0530

    HTML & JS Code Update

--- 27_JS_DOM_Task_List_App/app.js
+++ 27_JS_DOM_Task_List_App/app.js
@@ -13,4 +13,26 @@ taskFormEl.addEventListener('submit', function(event){
     
     // set to local storage
     localStorage.setItem('tasks', JSON.stringify(taskList));
+
+    displayTasks();
 });
+
+// display tasks
+let displayTasks = () => {
+    let taskListEl = document.querySelector('#task-list');
+    let taskList = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+    if(taskList.length !== 0){
+        let eachTask = '';
+        for(let task of taskList){
+            eachTask += `<li class="list-group-item list-group-item-action list-group-item-warning d-flex flex-row justify-content-between">
+                            <span class="font-weight-bold">${task}</span>
+                            <button class="close" style="border: 0px; padding: 0px; ">
+                                <i class="fa fa-window-close"></i>
+                            </button>
+                        </li>`;
+        }
+        taskListEl.innerHTML = eachTask;
+    }
+};
+
+displayTasks();
