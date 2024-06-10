2cd4d8e098965ae52ec734273c5d7674929efcc4
SRIRAJKUMAR K <ksrirajk@gmail.com>
2024-06-10 13:14:37 +0530

    JS Sub Classes Code Update

--- 31_JS_Sub_Classes/app.js
+++ 31_JS_Sub_Classes/app.js
@@ -24,5 +24,58 @@ class Person {
     }
 };
 
-let person = new Person('John', 'Doe');
-person.greet();
+// let person = new Person('John', 'Doe');
+// person.greet();
+
+
+class Employee extends Person{
+    constructor(firstName, lastName, age, designation){
+        super(firstName, lastName);
+        this.age = age;
+        this.designation = designation;
+    };
+
+    getAge(){
+        return this.age;
+    }
+
+    getDesignation(){
+        return this.designation;
+    }
+
+    greet(){
+        let msg = `Hello Mr: ${this.getFirstName()} ${this.getLastName()}
+                   AGE : ${this.getAge()}
+                   DESIGNATION : ${this.getDesignation()}`;
+        console.log(msg);
+    }
+};
+
+let employee = new Employee('John', 'Doe', 40, 'Manager');
+employee.greet();
+
+class Customer extends Person{
+    constructor(firstName, lastName, age, location){
+        super(firstName, lastName);
+        this.age = age;
+        this.location = location;
+    };
+
+    getAge(){
+        return this.age;
+    }
+
+    getLocation(){
+        return this.location;
+    }
+
+    greet(){
+        let msg = `Hello Mr: ${this.getFirstName()} ${this.getLastName()}
+                   AGE : ${this.getAge()}
+                   LOCATION : ${this.getLocation()}`;
+        console.log(msg);
+    }
+};
+
+let customer = new Customer('Laura', 'Wilson', 28, 'USA');
+customer.greet();
