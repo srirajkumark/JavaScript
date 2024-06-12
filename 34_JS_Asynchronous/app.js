f7741de7a1495eaf3486d6b40d0f5d3a80cdb5a8
SRIRAJKUMAR K <ksrirajk@gmail.com>
2024-06-12 13:43:06 +0530

    HTML & JS Code Update

---
+++ 34_JS_Asynchronous/app.js
@@ -0,0 +1,14 @@
+// Asynchronous JS:
+// 1) Callbacks
+// 2) Promises
+// 3) Async-Await
+
+
+
+console.log('car 1');
+console.log('car 2');
+setTimeout(() => {
+    console.log('car 3');
+}, 3000)
+console.log('car 4');
+console.log('car 5');
