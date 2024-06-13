e85b8a431afee1122f012dd4ab35d533c586c83b
Srirajkumar <ksrirajk@email.com>
2024-06-13 23:27:07 +0530

    HTML & JS Code Update

--- 35_JS_Callbacks/app.js
+++ 35_JS_Callbacks/app.js
@@ -46,3 +46,35 @@ output = calculate(10, 20, (a, b) => {
 });
 console.log(output);
 
+
+
+
+
+// Realtime use of callback function:
+
+let employee = [{id : 1, name : 'John', age : 35},
+                {id : 2, name : 'Rajan', age : 25}
+];
+
+let createEmployee = (employee) => {
+    setTimeout(() => {
+        employees.push(employee);
+    }, 2000);
+};
+
+let getEmployees = () => {
+    setTimeout(() => {
+        let employeeRows = '';
+        employees.foreach((employee) => {
+            employeeRows += `<tr>
+                                <td>${employee.id}</td>
+                                <td>${employee.name}</td>
+                                <td>${employee.age}</td>
+                             </tr>`
+        });
+        document.querySelector('#table-body').innerHTML = employeeRows;
+    }, 1000);
+};
+
+createEmployee({id : 3, name : 'Wilson', age : 45});
+getEmployees();
