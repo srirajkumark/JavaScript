67c6dece0f442da52a2bc356586eeb34b436d3df
SRIRAJKUMAR K <ksrirajk@gmail.com>
2024-06-13 12:34:55 +0530

    Updated JS Code

--- 35_JS_Callbacks/app.js
+++ 35_JS_Callbacks/app.js
@@ -1,3 +1,48 @@
 // 1)Callback function
 // -------------------
 
+// Normal Program:-
+// let calculate = (a, b, operaton) => {
+//     let result = '';
+//     if (operaton === 'sum'){
+//         result = a + b;
+//     }
+//     if (operaton === 'mul'){
+//         result = a * b;
+//     }
+//     return result; 
+// };
+
+// let output = calculate(10, 20, 'sum');
+// console.log(output);
+
+// output = calculate(10, 20, 'mul');
+// console.log(output);
+
+
+
+// Using Callback:-
+let sum = (a, b) => {
+    return a + b;
+};
+
+let mul = (a, b) => {
+    return a * b;
+};
+
+let calculate = (a, b, callback) => {
+    let result = '';
+    return callback(a, b);
+};
+
+let output = calculate(10, 20, sum);
+console.log(output);
+
+output = calculate(10, 20, mul);
+console.log(output);
+
+output = calculate(10, 20, (a, b) => {
+    return a - b;
+});
+console.log(output);
+
