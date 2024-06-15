1d95f796cae0af3eecba35dd976be8f95b506701
SRIRAJKUMAR K <ksrirajk@gmail.com>
2024-06-15 17:15:02 +0530

    Updated JS Code

---
+++ 36_JS_Promises/app.js
@@ -0,0 +1,37 @@
+// promse concept wth callbacks
+
+let doTask = (success, failure) => {
+    // dong the task
+    let isDone = true;
+    if(isDone){
+        success('Task is Done');
+    }
+    else{
+        failure('Task is Not Done');
+    }
+};
+doTask((message) => {
+    console.log(message);
+}, (message) => {
+    console.error(message);
+});
+
+
+// Actual Promise Creation:-
+let cleanCamera = new Promise((resolve, reject) => {
+    // cleaning camera
+    let isDone = true;
+    if(isDone){
+        resolve('Claeaning is Done');
+    }
+    else{
+        reject('Cleaning is Not Done');
+    }
+});
+
+cleanCamera.then((message) => {
+    console.log(message);
+}).catch((message) => {
+    console.error(message);
+});
+
