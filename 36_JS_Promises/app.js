c72157bc980a31360c3d68c42fdc3a4f2b200e52
SRIRAJKUMAR K <ksrirajk@gmail.com>
2024-06-17 01:37:49 +0530

    Updated JS Code

--- 36_JS_Promises/app.js
+++ 36_JS_Promises/app.js
@@ -35,3 +35,85 @@ cleanCamera.then((message) => {
     console.error(message);
 });
 
+
+
+// Dependent Promises:-
+
+// -> buildProject
+//  -> attendTechnicalRound
+//   -> attendManagerRound
+//    -> attendHRRound
+
+let buildProject = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let isDone = true;
+        if(isDone){
+            resolve('Project is Finished');
+        }
+        else{
+            reject('Project is Not Finished');
+        }
+    }, 1000);
+});
+
+let attendTechnicalRound = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let isDone = true;
+        if(isDone){
+            resolve('Technical Round is Finished');
+        }
+        else{
+            reject('Technical Round is Not Finished');
+        }
+    }, 2000);
+});
+
+let attendManagerRound = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let isDone = true;
+        if(isDone){
+            resolve('Manager Round is Finished');
+        }
+        else{
+            reject('Manager Round is Not Finished');
+        }
+    }, 3000);
+});
+
+let attendHRRound = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let isDone = true;
+        if(isDone){
+            resolve('HR Round is Finished, Got Job');
+        }
+        else{
+            reject('HR Round is Not Finished');
+        }
+        }, 4000);
+});
+
+buildProject.then((message) => {
+    let result = `${message} -> `;
+    console.log(message);   
+    attendTechnicalRound.then((message) => {
+        result += `${message} -> `;
+        console.log(message);
+        attendManagerRound.then((message) => {
+            result += `${message} -> `;
+            console.log(message);
+            attendHRRound.then((message) => {
+                result += `${message}`;
+                console.log(message);
+                // console.log(result);
+            }).catch((err) => {
+                console.error(err);
+            });
+        }).catch((err) => {
+            console.error(err);
+        });
+    }).catch((err) => {
+        console.error(err);
+    });
+}).catch((err) => {
+    console.error(err);
+});
